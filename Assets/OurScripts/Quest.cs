602f516c0a02ddb31ec848a70baf3c65de34b478
juliusg13 <juliusg13@ru.is>
2016-09-30 17:53:25 +0000

    Created Bounching explamation mark
    which can be clicked and destroyed, later it will be connected to
    Audur's question form which will bring up a question and disable the
    camera drag.

---
+++ Assets/OurScripts/Quest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quest : MonoBehaviour {
+
+	public GameObject quest;
+	public float xCoord;
+	public float zCoord;
+
+	// Use this for initialization
+	void Start () {
+		quest = GameObject.FindGameObjectWithTag("QuestGiver");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3 (xCoord, this.transform.position.y, zCoord);
+	}
+	void OnMouseUp(){
+		if (this.gameObject.tag == "QuestGiver") {
+			Destroy (this.gameObject);
+		}
+	}
+}
