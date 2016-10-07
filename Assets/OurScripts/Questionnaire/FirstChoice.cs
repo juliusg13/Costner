8b363afd2d9d9aa62d887c8488de4d6440472060
auduro12 <auduro12@ru.is>
2016-10-07 16:37:57 +0000

    Questions
    Fimm spurningar og svör, mjög hrátt

---
+++ Assets/OurScripts/Questionnaire/FirstChoice.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; 
+public class FirstChoice : MonoBehaviour {
+
+    List<string> firstChoice = new List<string>() { "Reykjavík", "20. ágúst 1800" , "Um 2000m", "Jón Jónsson", "Dönsku" };
+    // Use this for initialization
+    void Start()
+    {
+     //   GetComponent<TextMesh>().text = firstChoice[0];
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        if (TextControl.randQuestions > -1)
+        {
+            GetComponent<TextMesh>().text = firstChoice[TextControl.randQuestions];
+        }
+
+    }
+
+
+    void OnMouseDown()
+    {
+        TextControl.selectedAnswer = gameObject.name;
+        TextControl.choiceSelected = "y";
+        //Debug.Log(gameObject.name); 
+    }
+}
