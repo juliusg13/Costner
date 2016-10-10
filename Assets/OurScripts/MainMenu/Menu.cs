63cd34dd5d5fc8bfd28983ec2ad9aafc2c1816a1
gunnarcool <gunnarea@gmail.com>
2016-10-10 16:48:26 +0000

    MainMenu
    Created a simple menu with play, exit and options

---
+++ Assets/OurScripts/MainMenu/Menu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+    public Canvas MainCanvas;
+
+    public void QuitOn()
+    {
+        Application.Quit();
+    }
+	
+    public void PlayOn()
+    {
+        SceneManager.LoadScene("Test");
+    }
+}
