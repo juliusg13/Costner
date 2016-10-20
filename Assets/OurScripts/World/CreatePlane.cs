f66743976e30430bf33a173d62a319666df16630
gunnarcool <gunnarea@gmail.com>
2016-10-20 11:39:46 +0000

    Mouse drag upgrade
    Mouse can now drag left, right and down. but not up

--- Assets/OurScripts/World/CreatePlane.cs
+++ Assets/OurScripts/World/CreatePlane.cs
@@ -25,31 +25,35 @@ public class CreatePlane : MonoBehaviour {
     [Header("Unity coordinates X of the map")]
     public int maxTilePosX = 10;
     public int minTilePosX = 0;
+    public int maxTilePosY;
+    public int minTilePosY;
 
     private GameObject cam;
     private double camPosX;
     private double camPosY;
+    
 
     private GameObject plane;
 
     // Use this for initialization
     void Start ()
     {
-        CreateMap();
         cam = GameObject.Find("Main Camera");
+        camPosX = cam.GetComponent<Mouse>().cameraStartPosX;
+        camPosY = cam.GetComponent<Mouse>().cameraStartPosY;
+        CreateMap();
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        camPosX = cam.transform.position.x;
+        camPosY = cam.transform.position.z;
         addRowOfTiles();
 	}
 
     void addRowOfTiles()
     {
-        camPosX = cam.transform.position.x;
-        camPosY = cam.transform.position.z;
-
         if(maxTileX >= totalTilesZoomX)
         {
             maxTileX = 0;
@@ -66,24 +70,7 @@ public class CreatePlane : MonoBehaviour {
         {
             minTileX = totalTilesZoomX - 1;
         }
-
-        /*if (camPosX + 4 > maxTilePosX)
-        {
-            for(int i = 0; i < totalTilesY; i++)
-            {
-                plane = new GameObject();
-                plane.transform.parent = GameObject.Find("TileHolder").transform;
-                plane.name = maxTilePosX + ", " + i;
-                plane.AddComponent<CreateTile>();
-                plane.GetComponent<CreateTile>().posX = maxTilePosX;
-                plane.GetComponent<CreateTile>().posY = -i;
-                StartCoroutine(GetTextureMap(plane, maxTileX, i));
-            }
-            maxTileX++;
-            totalTilesX++;
-            maxTilePosX++;
-        }*/
-
+        //Left
         if (camPosX - 4 < minTilePosX)
         {
             for (int i = minTileY; i <= maxTileY; i++)
@@ -100,18 +87,61 @@ public class CreatePlane : MonoBehaviour {
             totalTilesX++;
             minTilePosX--;
         }
-
+        //Right
+        if (camPosX + 4 > maxTilePosX)
+        {
+            for (int i = maxTileY; i >= minTileY; i--)
+            {
+                plane = new GameObject();
+                plane.transform.parent = GameObject.Find("TileHolder").transform;
+                plane.name = maxTilePosX + ", " + i;
+                plane.AddComponent<CreateTile>();
+                plane.GetComponent<CreateTile>().posX = maxTilePosX;
+                plane.GetComponent<CreateTile>().posY = -i;
+                StartCoroutine(GetTextureMap(plane, maxTileX, i));
+            }
+            maxTileX++;
+            totalTilesX++;
+            maxTilePosX++;
+        }
+        //Down
         if (camPosY - 4 < -maxTileY)
         {
-            for (int i = minTilePosX+1; i <= maxTilePosX; i++)
+            for (int i = minTilePosX + 1; i <= maxTilePosX; i++)
             {
+                int j = i;
+                if(j < 0)
+                {
+                    j = (j % totalTilesZoomX) + totalTilesZoomX;
+                }
                 plane = new GameObject();
                 plane.transform.parent = GameObject.Find("TileHolder").transform;
                 plane.name = i + ", " + maxTileY;
                 plane.AddComponent<CreateTile>();
                 plane.GetComponent<CreateTile>().posX = i;
                 plane.GetComponent<CreateTile>().posY = -maxTileY;
-                StartCoroutine(GetTextureMap(plane, i, maxTileY));
+                StartCoroutine(GetTextureMap(plane, j, maxTileY));
+            }
+            maxTileY++;
+            totalTilesY++;
+        }
+        //Up
+        if (camPosY + 4 > -minTileY)
+        {
+            for (int i = minTilePosX + 1; i <= maxTilePosX; i++)
+            {
+                int j = i;
+                if (j < 0)
+                {
+                    j = (j % totalTilesZoomX) + totalTilesZoomX;
+                }
+                plane = new GameObject();
+                plane.transform.parent = GameObject.Find("TileHolder").transform;
+                plane.name = i + ", " + maxTileY;
+                plane.AddComponent<CreateTile>();
+                plane.GetComponent<CreateTile>().posX = i;
+                plane.GetComponent<CreateTile>().posY = -maxTileY;
+                StartCoroutine(GetTextureMap(plane, j, maxTileY));
             }
             maxTileY++;
             totalTilesY++;
@@ -120,7 +150,7 @@ public class CreatePlane : MonoBehaviour {
 
     void CreateMap()
     {
-        if(zoom == 0)
+        if (zoom == 0)
         {
             totalTilesZoomX = 1;
         }
@@ -133,19 +163,30 @@ public class CreatePlane : MonoBehaviour {
             }
             totalTilesZoomY = totalTilesZoomX;
         }
-        for(int i = 0; i <= startTilesX; i++)
+        for(int i = Convert.ToInt32(camPosX) - (startTilesX / 2); i <= Convert.ToInt32(camPosX) + (startTilesX / 2); i++)
         {
-            for(int j = 0; j <= startTilesY; j++)
+            for(int j = Convert.ToInt32(camPosY) - (startTilesY / 2); j <= Convert.ToInt32(camPosY) + (startTilesY / 2); j++)
             {
                 plane = new GameObject();
                 plane.transform.parent = GameObject.Find("TileHolder").transform;
                 plane.name = i + ", " + j;
                 plane.AddComponent<CreateTile>();
                 plane.GetComponent<CreateTile>().posX = i;
-                plane.GetComponent<CreateTile>().posY = -j;
-                StartCoroutine(GetTextureMap(plane, i, j));
+                plane.GetComponent<CreateTile>().posY = j;
+                StartCoroutine(GetTextureMap(plane, i, -j));
             }
         }
+
+        minTilePosX = Convert.ToInt32(camPosX) - (startTilesX / 2);
+        maxTilePosX = Convert.ToInt32(camPosX) + (startTilesX / 2);
+        minTilePosY = Convert.ToInt32(camPosY) - (startTilesY / 2);
+        maxTilePosY = Convert.ToInt32(camPosY) + (startTilesY / 2);
+
+        maxTileX = (Math.Abs(Convert.ToInt32(camPosX))) + (startTilesX / 2) % totalTilesZoomX;
+        minTileX = (Math.Abs(Convert.ToInt32(camPosX))) - (startTilesX / 2) % totalTilesZoomX;
+        maxTileY = (Math.Abs(Convert.ToInt32(camPosY))) + (startTilesY / 2) % totalTilesZoomY;
+        minTileY = (Math.Abs(Convert.ToInt32(camPosY))) - (startTilesY / 2) % totalTilesZoomY;
+
         totalTilesX = startTilesX;
         totalTilesY = startTilesY;
     }
