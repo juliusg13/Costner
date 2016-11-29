743b849ca36df901d42797c83077307bbc334810
juliusg13 <juliusg13@ru.is>
2016-11-29 11:23:05 +0000

    patch

---
+++ Assets/OurScripts/quest.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class quest : MonoBehaviour {
+
+	public GameObject questGiver;
+    public string questionID;
+	public float xCoord;
+	public float zCoord;
+    private GameObject controller;
+    
+
+
+    // Use this for initialization
+    void Start () {
+		questGiver = GameObject.FindGameObjectWithTag("QuestGiver");
+        controller = GameObject.FindWithTag("GameController");
+
+    }
+    
+    // Update is called once per frame
+    void Update () {
+		this.transform.position = new Vector3 (xCoord, this.transform.position.y, zCoord);
+	}
+	void OnMouseUp(){
+		if (this.gameObject.tag == "QuestGiver") {
+            //Destroy (this.gameObject);
+            //window.GetComponent<questionWindow>().ShowWindow();
+          //  controller.GetComponent<qWindowDB>().ShowWindow();
+			controller.GetComponent<qWindowDB> ().ShowWindow (questionID);
+        }
+	}
+
+}
