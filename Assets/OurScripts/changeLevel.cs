2c31974295b503c0ec1d1c7d573bb4944d57b105
Hulda Peturs <huldakp@gmail.com>
2016-12-01 18:06:28 +0000

    change level og start game
    importaði assets, bættust við einhverjir mapzengo.mat filear.

--- Assets/OurScripts/changeLevel.cs
+++ Assets/OurScripts/changeLevel.cs
@@ -1,9 +1,19 @@
-﻿/*using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class changeLevel : MonoBehaviour {
 
-	public float levelStartDealy = 2f; 	public Canvas levels;   	private Button levelsButton; 	private Button level1Button; 	private Button level2Button; 	private Button level3Button;  	private Text levelText; 	private GameObject levelImage; 	private int level = 1; 	private bool doingSetup;   	private void levelUp (int index) {  		level++; 		initGame(); 	} 	// displays the level image when clicked 	void initGame () {  		doingSetup = true; 		levelImage = GameObject.Find("LevelImage"); 		levelText = GameObject.Find("LevelsButton").GetComponents<Button>(); 		levelImage.SetActive(true);  		 	} 	// hide the level image when game continues 	private void hideLevelImage () {  		levelImage.SetActive(false); 		doingSetup = false; 	} 	// Use this for initialization 	/*void Start () { 		 		level1 = true;  	} 	 	// Update is called once per frame 	void Update () { 	 	} */ //}  
+	public GameObject levelImage;
 
+	public void Loadlevel(bool changeLevel) {
+
+		levelImage.SetActive(true);
+	}
+	// Sends you back to continue playing the game
+	public void BackToGame(bool scene) {
+
+		levelImage.SetActive(false);
+    }
+}
