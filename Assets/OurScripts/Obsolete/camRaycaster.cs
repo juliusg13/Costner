3ee727e1ad9337adb2babe5220e94d1ed8469cb6
juliusg13 <juliusg13@ru.is>
2016-12-12 17:56:05 +0000

    super kewl bug fixes and nextquestion
    NextQ is relative to screen size and does not jump away if question is
    far away.

---
+++ Assets/OurScripts/Obsolete/camRaycaster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camRaycaster : MonoBehaviour {
+    GameObject cam;
+    Camera camera;
+
+    void Start() {
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+        camera = cam.GetComponent<Camera>();
+    }
+
+    void Update() {
+
+    }
+    public void findTileLocation() {
+        Ray ray = camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit)) {
+            print("I'm looking at " + hit.transform.parent.name);
+
+        }
+        else
+            print("I'm looking at nothing!");
+
+    }
+}
--- Assets/OurScripts/getRandomQuestion.cs
+++ Assets/OurScripts/getRandomQuestion.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class getRandomQuestion : MonoBehaviour {
-    GameObject controller, questionObject, camera;
+    GameObject controller, questionObject, camera, world;
     string questionName;
-    int linkedListCount, random;
+    int linkedListCount, random, zoom;
+    public int numberOfNewWorldsDistance;
+    float camX, camZ, x, z, absX, absZ, distance, maxDistance;
     string qID;
 	// Use this for initialization
 	void Start () {
         controller = GameObject.FindWithTag("GameController");
         camera = GameObject.FindWithTag("MainCamera");
+        world = GameObject.FindWithTag("OpenWorld");
         linkedListCount = 0;
         random = 0;
+        numberOfNewWorldsDistance = 3;
 	}
 	
 	// Update is called once per frame
@@ -43,9 +47,38 @@ public class getRandomQuestion : MonoBehaviour {
     }
 
     void moveCamera(GameObject q) {
-        float x = q.gameObject.transform.position.x;
-        float z = q.gameObject.transform.position.z;
-        //float y = camera.gameObject.transform.position.y;
-        camera.gameObject.transform.position = new Vector3(x, 160f, z);
+
+        camX = camera.transform.position.x;
+        camZ = camera.transform.position.z;
+        x = q.gameObject.transform.position.x;
+        z = q.gameObject.transform.position.z;
+        absX = Mathf.Abs(camX - x);
+        absZ = Mathf.Abs(camZ - z);
+        //print("AbsX : " + absX + " AbsZ : " + absZ);
+
+        distance = Mathf.Sqrt(Mathf.Pow(absX, 2) + Mathf.Pow(absZ, 2));
+        world = GameObject.FindWithTag("OpenWorld");
+        zoom = world.GetComponent<MapzenGo.Models.TileManager>().Zoom;
+        setMaxDistance(zoom);
+        print("dist: " + distance);
+        print("maxdist : " + maxDistance);
+        if (distance > maxDistance) {
+            print("LOLSUCKSTO BE YOU NIGGA");
+        } else {
+            camera.gameObject.transform.position = new Vector3(x, 160f, z);
+        }
+    }
+
+    void setMaxDistance(int zoom) {
+        if(zoom == 16) {
+            maxDistance = numberOfNewWorldsDistance * 1 * 1834; //new worlds * scale due to zoom * 1834 is dist on new worlds.
+        } else if(zoom == 15) {
+            maxDistance = numberOfNewWorldsDistance * 2 * 1834;
+        } else if(zoom == 14) {
+            maxDistance = numberOfNewWorldsDistance * 4 * 1834;
+        } else {
+            maxDistance = 0;
+        }
+        
     }
 }
